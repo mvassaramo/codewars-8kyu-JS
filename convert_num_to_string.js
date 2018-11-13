2b212c8a4be1300d727722281bc1bf1ac850178e
github username <github email address>
2018-11-13 20:15:37 +0000

    solved number to string kata

---
+++ convert_num_to_string.js
@@ -0,0 +1,15 @@
+// We need a function that can transform a number into a string.
+//
+// What ways of achieving this do you know?
+//
+// Examples:
+// numberToString(123); // returns '123';`
+// numberToString(999); // returns '999';`
+
+
+
+const numberToString = num => num.toString()
+
+
+//another solution
+// return String(num);
